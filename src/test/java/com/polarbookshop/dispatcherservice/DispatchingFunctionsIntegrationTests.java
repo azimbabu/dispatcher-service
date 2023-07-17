9f5215fdfc01cb8acaca134db1d092b8bbdbe613
azimbabu <azim.babu@gmail.com>
2023-07-17 00:01:07 -0700

    fix tests

--- src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
+++ src/test/java/com/polarbookshop/dispatcherservice/DispatchingFunctionsIntegrationTests.java
@@ -1,13 +1,16 @@
 package com.polarbookshop.dispatcherservice;
 
-import org.assertj.core.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.function.context.FunctionCatalog;
 import org.springframework.cloud.function.context.test.FunctionalSpringBootTest;
+import org.springframework.messaging.Message;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.io.IOException;
 import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,6 +19,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DispatchingFunctionsIntegrationTests {
     @Autowired
     private FunctionCatalog catalog;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void packOrder() {
@@ -34,12 +40,35 @@ public class DispatchingFunctionsIntegrationTests {
     }
 
     @Test
+    @Disabled
     void packAndLabelOrder() {
-        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>>
-                packAndLabel = catalog.lookup(Function.class, "pack|label");
+        Function<OrderAcceptedMessage, Flux<OrderDispatchedMessage>> packAndLabel =
+                catalog.lookup(Function.class, "pack|label");
         long orderId = 121;
+
         StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
-                .expectNextMatches(dispatchedOrder -> dispatchedOrder.equals(new OrderDispatchedMessage(orderId)))
+                .expectNextMatches(dispatchedOrder ->
+                        dispatchedOrder.equals(new OrderDispatchedMessage(orderId)))
                 .verifyComplete();
     }
+
+    @Test
+    void packAndLabelOrder2() {
+        Function<OrderAcceptedMessage, Flux<Message<byte[]>>>
+                packAndLabel = catalog.lookup(Function.class, "pack|label");
+        long orderId = 121;
+
+        StepVerifier.create(packAndLabel.apply(new OrderAcceptedMessage(orderId)))
+                .expectNextMatches(dispatchedOrder -> {
+                    try {
+                        OrderDispatchedMessage orderDispatchedMessage = objectMapper.readValue(dispatchedOrder.getPayload(), OrderDispatchedMessage.class);
+                        return orderDispatchedMessage.equals(new OrderDispatchedMessage(orderId));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .verifyComplete();
+    }
+
+
 }
--- src/test/java/com/polarbookshop/dispatcherservice/FunctionsStreamIntegrationTests.java
+++ src/test/java/com/polarbookshop/dispatcherservice/FunctionsStreamIntegrationTests.java
@@ -1,7 +1,6 @@
 package com.polarbookshop.dispatcherservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
